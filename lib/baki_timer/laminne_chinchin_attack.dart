f57a48eceb5204881322148c39364065100caf97
isso0424 <isso04240129@gmail.com>
2020-10-10 00:36:01 +0900

    [Add]Defined baki and ultra

--- lib/baki_timer/laminne_chinchin_attack.dart
+++ lib/baki_timer/laminne_chinchin_attack.dart
@@ -8,6 +8,51 @@ class LammineChinchinAttack extends StatefulWidget {
 class LammineChinchinAttackState extends State<LammineChinchinAttack> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+
+            },
+            child: Text("刃牙(1min)"),
+          ),
+          RaisedButton(
+            onPressed: () {
+
+            },
+            child: Text("ウルトラマン(3min)"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Baki extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => BakiState();
+}
+
+class BakiState extends State<Baki> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class Ultra extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class UltraState extends State<Ultra> {
+  @override
+  Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
